737adaff7e7f5f3ac02154b11bf9c61f660bf74d
JngMkk <kjm0817ss@gmail.com>
2023-12-06 02:17:50 +0900

    expl: programmers 42627

--- Exercise/Programmers/Python/Heap/42627.py
+++ Exercise/Programmers/Python/Heap/42627.py
@@ -1,65 +1,39 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42627
+def solution(jobs: list[list[int]]):
+    from heapq import heappop, heappush
 
-"""
-문제 설명
-하드디스크는 한 번에 하나의 작업만 수행할 수 있습니다. 디스크 컨트롤러를 구현하는 방법은 여러 가지가 있습니다. 가장 일반적인 방법은 요청이 들어온 순서대로 처리하는 것입니다.
+    heap_q = []  # exec time이 짧은 순으로 대기 큐 생성
+    time_total = 0  # process waiting time + process exec time
+    time_now = -1  # 현재 하드 디스크 I/O 작업이 없는 경우로 초기화
 
-예를들어
-
-- 0ms 시점에 3ms가 소요되는 A작업 요청
-- 1ms 시점에 9ms가 소요되는 B작업 요청
-- 2ms 시점에 6ms가 소요되는 C작업 요청
-
-한 번에 하나의 요청만을 수행할 수 있기 때문에 각각의 작업을 요청받은 순서대로 처리하면 다음과 같이 처리 됩니다.
-
-- A: 3ms 시점에 작업 완료 (요청에서 종료까지 : 3ms)
-- B: 1ms부터 대기하다가, 3ms 시점에 작업을 시작해서 12ms 시점에 작업 완료(요청에서 종료까지 : 11ms)
-- C: 2ms부터 대기하다가, 12ms 시점에 작업을 시작해서 18ms 시점에 작업 완료(요청에서 종료까지 : 16ms)
-이 때 각 작업의 요청부터 종료까지 걸린 시간의 평균은 10ms(= (3 + 11 + 16) / 3)가 됩니다.
-
-하지만 A → C → B 순서대로 처리하면
-
-- A: 3ms 시점에 작업 완료(요청에서 종료까지 : 3ms)
-- C: 2ms부터 대기하다가, 3ms 시점에 작업을 시작해서 9ms 시점에 작업 완료(요청에서 종료까지 : 7ms)
-- B: 1ms부터 대기하다가, 9ms 시점에 작업을 시작해서 18ms 시점에 작업 완료(요청에서 종료까지 : 17ms)
-이렇게 A → C → B의 순서로 처리하면 각 작업의 요청부터 종료까지 걸린 시간의 평균은 9ms(= (3 + 7 + 17) / 3)가 됩니다.
-
-각 작업에 대해 [작업이 요청되는 시점, 작업의 소요시간]을 담은 2차원 배열 jobs가 매개변수로 주어질 때, 작업의 요청부터 종료까지 걸린 시간의 평균을 가장 줄이는 방법으로 처리하면 평균이 얼마가 되는지 return 하도록 solution 함수를 작성해주세요. (단, 소수점 이하의 수는 버립니다)
-
-제한 사항
-jobs의 길이는 1 이상 500 이하입니다.
-jobs의 각 행은 하나의 작업에 대한 [작업이 요청되는 시점, 작업의 소요시간] 입니다.
-각 작업에 대해 작업이 요청되는 시간은 0 이상 1,000 이하입니다.
-각 작업에 대해 작업의 소요시간은 1 이상 1,000 이하입니다.
-하드디스크가 작업을 수행하고 있지 않을 때에는 먼저 요청이 들어온 작업부터 처리합니다.
-입출력 예
-jobs	return
-[[0, 3], [1, 9], [2, 6]]	9
-입출력 예 설명
-문제에 주어진 예와 같습니다.
-
-0ms 시점에 3ms 걸리는 작업 요청이 들어옵니다.
-1ms 시점에 9ms 걸리는 작업 요청이 들어옵니다.
-2ms 시점에 6ms 걸리는 작업 요청이 들어옵니다.
-"""
-
-import heapq
-
-
-def solution(jobs):
-    n = len(jobs)
     jobs.sort(reverse=True)
-    res, now = 0, 0
-    hq = []
+    N = len(jobs)
 
-    while jobs or hq:
-        if jobs and jobs[-1][0] <= now:
-            j = jobs.pop()
-            heapq.heappush(hq, (j[1], j[0]))
-        elif hq:
-            j = heapq.heappop(hq)
-            res += now + j[0] - j[1]
-            now += j[0]
-        else:
-            now = jobs[-1][0]
-    return res // n
+    while jobs or heap_q:
+        if jobs and jobs[-1][0] <= time_now:  # 요청이 I/O 작업 중에 들어온 경우
+            req_time, exec_time = jobs.pop()  # 요청 시점이 가장 가까운 process 꺼냄
+            heappush(heap_q, (exec_time, req_time))  # 대기 큐에 삽입
+        elif heap_q:  # 대기 큐에 process가 있다면
+            exec_time, req_time = heappop(heap_q)
+            time_total += time_now - req_time + exec_time  # 현재 시점 - 요청 시점 + 실행 시간
+            time_now += exec_time  # 현재 시점에서 실제 실행 시간 덧셈
+        else:  # 현재 하드 디스크 I/O 작업이 없을 경우 다음 요청 시점으로 현재 시점을 update (I/O 작업 중으로)
+            time_now = jobs[-1][0]
+
+    return time_total // N  # 평균 및 소수점 버리기
+
+
+"""
+1. sort() 메서드: O(N * log N)
+2. while loop
+    - heappush(): O(log M)
+    - heappop(): O(log M)
+    - 루프의 총 시간 복잡도는 작업 목록 길이 N과 heap queue의 연산에 의해 결정됨.
+    - heap queue의 최대 크기는 N이므로, 각 힙 연산의 복잡도는 최대 O(log N)
+    - 따라서 이 루프의 총 시간 복잡도는 O(N * log N)
+
+전체 시간 복잡도: O(N * log N)
+"""
+
+
+jobs = [[0, 3], [1, 9], [2, 6]]
+print(solution(jobs))
