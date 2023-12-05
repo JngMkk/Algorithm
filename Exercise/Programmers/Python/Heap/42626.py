72f39458cb2844a2ec99d42c27364c637ee25045
JngMkk <kjm0817ss@gmail.com>
2023-12-05 13:48:25 +0900

    expl: programmers 42626

--- Exercise/Programmers/Python/Heap/42626.py
+++ Exercise/Programmers/Python/Heap/42626.py
@@ -1,48 +1,36 @@
-# https://school.programmers.co.kr/learn/courses/30/lessons/42626
-
-"""
-문제 설명
-매운 것을 좋아하는 Leo는 모든 음식의 스코빌 지수를 K 이상으로 만들고 싶습니다. 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 Leo는 스코빌 지수가 가장 낮은 두 개의 음식을 아래와 같이 특별한 방법으로 섞어 새로운 음식을 만듭니다.
-
-섞은 음식의 스코빌 지수 = 가장 맵지 않은 음식의 스코빌 지수 + (두 번째로 맵지 않은 음식의 스코빌 지수 * 2)
-Leo는 모든 음식의 스코빌 지수가 K 이상이 될 때까지 반복하여 섞습니다.
-Leo가 가진 음식의 스코빌 지수를 담은 배열 scoville과 원하는 스코빌 지수 K가 주어질 때, 모든 음식의 스코빌 지수를 K 이상으로 만들기 위해 섞어야 하는 최소 횟수를 return 하도록 solution 함수를 작성해주세요.
-
-제한 사항
-scoville의 길이는 2 이상 1,000,000 이하입니다.
-K는 0 이상 1,000,000,000 이하입니다.
-scoville의 원소는 각각 0 이상 1,000,000 이하입니다.
-모든 음식의 스코빌 지수를 K 이상으로 만들 수 없는 경우에는 -1을 return 합니다.
-입출력 예
-scoville	K	return
-[1, 2, 3, 9, 10, 12]	7	2
-입출력 예 설명
-스코빌 지수가 1인 음식과 2인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
-새로운 음식의 스코빌 지수 = 1 + (2 * 2) = 5
-가진 음식의 스코빌 지수 = [5, 3, 9, 10, 12]
-
-스코빌 지수가 3인 음식과 5인 음식을 섞으면 음식의 스코빌 지수가 아래와 같이 됩니다.
-새로운 음식의 스코빌 지수 = 3 + (5 * 2) = 13
-가진 음식의 스코빌 지수 = [13, 9, 10, 12]
-
-모든 음식의 스코빌 지수가 7 이상이 되었고 이때 섞은 횟수는 2회입니다.
-"""
-
-import heapq
-
-
-def solution(scoville, K):
-    heapq.heapify(scoville)
+def solution(scoville: list[int], K: int) -> int:
+    from heapq import heapify, heappop, heappush
 
     cnt = 0
-    while scoville and scoville[0] < K:
-        sco1 = heapq.heappop(scoville)
-        if not scoville:
-            return -1
-        sco2 = heapq.heappop(scoville)
-
-        sco = sco1 + sco2 * 2
-        heapq.heappush(scoville, sco)
+    heapify(scoville)
+    while True:
+        least = heappop(scoville)
+        if least >= K:
+            break
+        _next = heappop(scoville)
+        heappush(scoville, least + _next * 2)
         cnt += 1
 
     return cnt
+
+
+"""
+list의 길이를 N이라고 했을 때,
+
+heapify(list): 리스트를 힙으로 변환 => 시간 복잡도 O(N)
+heappop(list): 최소 힙에서 최소 요소를 제거하고 반환 => O(log N)
+heappush(list, value): 요소를 힙에 추가 => O(log N)
+
+while loop에서 heappop은 두 번, heappush는 한 번 호출됨.
+loop 반복의 총 시간 복잡도: O(3log N) => O(log N)
+loop는 최악의 경우 (N - 1)번 반복될 수 있음. (모든 요소를 한 번씩 처리했을 때)
+따라서, loop의 전체 시간 복잡도: O(Nlog N)
+
+전체 시간 복잡도: heapify(list)의 O(N)과 while loop의 O(Nlog N)을 합한 것 => O(Nlog N)
+입력 리스트의 크기에 따라 로그 선형적으로 증가하는 복잡도를 가짐
+"""
+
+
+scoville = [1, 2, 3, 9, 10, 12]
+K = 7
+print(solution(scoville, K))
